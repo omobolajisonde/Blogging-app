d01bf2d681f5340ec6e36a71b55dcdc6caed920e
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2023-01-04 22:12:33 +0100

    opt: wrapped the auth  controllers using the catchAsync function.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,69 +1,62 @@
 const crypto = require("crypto");
 
 const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
 const emailSender = require("../utils/emaliSender");
 const genToken = require("../utils/genToken");
 
-exports.signUpUser = async (req, res, next) => {
-  try {
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password,
-      confirmPassword,
-    });
-    user.password = undefined; // so the password won't show in the output and as payload in the token
-    user.__v = undefined;
-    const token = genToken(user);
-    return res.status(201).json({
-      status: "success",
-      token,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.signUpUser = catchAsync(async (req, res, next) => {
+  const { firstName, lastName, email, password, confirmPassword } = req.body;
+  const user = await User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+  });
+  user.password = undefined; // so the password won't show in the output and as payload in the token
+  user.__v = undefined;
+  const token = genToken(user);
+  return res.status(201).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+});
 
-exports.signInUser = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (!email || !password)
-      return next(new Error("Bad request! Email and Password is required."));
-    const user = await User.findOne({ email }).select("+password");
-    if (!user || !(await user.isCorrectPassword(password)))
-      return next(new Error("Unauthenticated! Email or Password incorrect."));
-    user.password = undefined;
-    user.__v = undefined;
-    const token = genToken(user);
-    return res.status(200).json({
-      status: "success",
-      token,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+exports.signInUser = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return next(new Error("Bad request! Email and Password is required."));
+  const user = await User.findOne({ email }).select("+password");
+  if (!user || !(await user.isCorrectPassword(password)))
+    return next(new Error("Unauthenticated! Email or Password incorrect."));
+  user.password = undefined;
+  user.__v = undefined;
+  const token = genToken(user);
+  return res.status(200).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+});
 
-exports.forgotPassword = async (req, res, next) => {
+exports.forgotPassword = catchAsync(async (req, res, next) => {
   const { email } = req.body;
   const user = await User.findOne({ email });
   if (!user) return next(new Error("User does not exist!"));
+  const resetToken = user.genResetToken();
+  user.save({ validateBeforeSave: false }); // persists the changes made in  genResetToken function
+  const resetPasswordURL = `${req.protocol}://${req.get(
+    "host"
+  )}/api/v1/auth/resetPassword/${resetToken}`;
+  const body = `Forgot your password? Submit a PATCH request with your new password and confirmPassword to: <a href=${resetPasswordURL}>${resetPasswordURL}</a>.\nIf you didn't forget your password, please ignore this email!`;
+  const subject = "Your password reset token (valid for 10 min)";
   try {
-    const resetToken = user.genResetToken();
-    user.save({ validateBeforeSave: false }); // persists the changes made in  genResetToken function
-    const resetPasswordURL = `${req.protocol}://${req.get(
-      "host"
-    )}/api/v1/auth/resetPassword/${resetToken}`;
-    const body = `Forgot your password? Submit a PATCH request with your new password and confirmPassword to: <a href=${resetPasswordURL}>${resetPasswordURL}</a>.\nIf you didn't forget your password, please ignore this email!`;
-    const subject = "Your password reset token (valid for 10 min)";
     await emailSender({ email, body, subject });
     return res.status(200).json({
       status: "success",
@@ -81,39 +74,35 @@ exports.forgotPassword = async (req, res, next) => {
       )
     );
   }
-};
+});
 
-exports.resetPassword = async (req, res, next) => {
-  try {
-    const token = req.params.token;
-    const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
-    // Looks for user with the reset token and unexpired!
-    const user = await User.findOne({
-      passwordResetToken: hashedToken,
-      passwordResetTokenExpiryTime: { $gt: Date.now() }, // this confirms that the token hasn't expired
-    });
-    if (!user)
-      return next(new Error("Password reset token is invalid or has expired!"));
-    const { password, confirmPassword } = req.body;
-    // Resets the password
-    user.password = password;
-    user.confirmPassword = confirmPassword;
-    // clears the passwordResetToken details on successful password update
-    user.passwordResetToken = undefined;
-    user.passwordResetTokenExpiryTime = undefined;
-    await user.save({ validateModifiedOnly: true }); // saves and update the passwordModifiedAt field
-    // 4) Log the user in, send JWT
-    user.password = undefined;
-    user.passwordModifiedAt = undefined;
-    const jwttoken = genToken(user);
-    return res.status(200).json({
-      status: "success",
-      token: jwttoken,
-      data: {
-        user,
-      },
-    });
-  } catch (error) {
-    return next(error);
-  }
-};
+exports.resetPassword = catchAsync(async (req, res, next) => {
+  const token = req.params.token;
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+  // Looks for user with the reset token and unexpired!
+  const user = await User.findOne({
+    passwordResetToken: hashedToken,
+    passwordResetTokenExpiryTime: { $gt: Date.now() }, // this confirms that the token hasn't expired
+  });
+  if (!user)
+    return next(new Error("Password reset token is invalid or has expired!"));
+  const { password, confirmPassword } = req.body;
+  // Resets the password
+  user.password = password;
+  user.confirmPassword = confirmPassword;
+  // clears the passwordResetToken details on successful password update
+  user.passwordResetToken = undefined;
+  user.passwordResetTokenExpiryTime = undefined;
+  await user.save({ validateModifiedOnly: true }); // saves and update the passwordModifiedAt field
+  // 4) Log the user in, send JWT
+  user.password = undefined;
+  user.passwordModifiedAt = undefined;
+  const jwttoken = genToken(user);
+  return res.status(200).json({
+    status: "success",
+    token: jwttoken,
+    data: {
+      user,
+    },
+  });
+});
