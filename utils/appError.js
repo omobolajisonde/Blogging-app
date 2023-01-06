2d565c95abbf31e17d702408fbe0ec561eb95f17
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2023-01-06 05:36:02 +0100

    feat: Implement custom error class

---
+++ utils/appError.js
@@ -0,0 +1,14 @@
+class AppError extends Error {
+  constructor(message, statusCode) {
+    super();
+    this.message = message;
+    this.statusCode = statusCode;
+    this.status = `${this.statusCode}`.startsWith("4")
+      ? "Client Error!"
+      : "Internal Server Error";
+    this.isOperational = true;
+    Error.captureStackTrace(this, this.constructor); // persists the function where the error occured in the Error stack trace. (not 100% sure)
+  }
+}
+
+module.exports = AppError;
